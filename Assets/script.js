c57b06d2bbf65fd6663edd70486fdc187ce7c7ce
Robert Bullfrog <williampikus@gmail.com>
2022-01-17 13:02:24 -0800

    JS complete, except function isnt populating schedule from local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-var currentDayText = $("#currentDay")
+var currentDayText = $("#currentDay");
 var saveBtn = $(".saveBtn");
 var container = $(".container");
 var textArea9am = $("#9am");
@@ -11,7 +11,7 @@ var textArea3pm = $("#3pm");
 var textArea4pm = $("#4pm");
 var textArea5pm = $("#5pm");
 
-var textAreaElArray = [
+var textAreaArray = [
     textArea9am,
     textArea10am,
     textArea11am,
@@ -23,31 +23,55 @@ var textAreaElArray = [
     textArea5pm,
 ];
 
-// Update time
+// Update time and textareas
 function updateTime() {
     var today = moment();
 
     // Sets current time in header
     currentDayText.text(today.format("dddd, MMMM Do YYYY, h:mm.ss"));
 
-    // Style timeblocks
+    // Style timeblocks based on hour
     var now = moment().format("k");
-    for (var i = 0; i < textAreaElArray.length; i++) {
-        textAreaElArray[i].removeClass("future past present");
+    for (var i = 0; i < textAreaArray.length; i++) {
+        textAreaArray[i].removeClass("future past present");
 
-        if (now > textAreaElArray[i].data("hour")) {
-            textAreaElArray[i].addClass("past");
+        if (now > textAreaArray[i].data("hour")) {
+            textAreaArray[i].addClass("past");
 
-        } else if (now == textAreaElArray[i].data("hour")) {
-            textAreaElArray[i].addClass("present");
+        } else if (now == textAreaArray[i].data("hour")) {
+            textAreaArray[i].addClass("present");
 
         } else {
 
-            textAreaElArray[i].addClass("future");
+            textAreaArray[i].addClass("future");
         }
     }
 }
 
+// Load schedule from local storage
+function loadTimeBlocks(){
+    for (var i=0; i < textAreaArray.length; i++){
+        textAreaArray[i].text = localStorage.getItem("time-block" + textAreaArray[i].data("hour"));
+    }
+}
+
+// save entry to local storage on save click
+function handleSave(event){
+    event.preventDefault();
+
+    // get element that was clicked
+    var btnClicked = $(event.currentTarget);
+    // get text of text area within the div of selected save btn
+    var targetText = btnClicked.siblings(".textarea");
+    // get the hour of selected
+    var targetHour = targetText.data("hour");
+
+    localStorage.setItem("time-block" + targetHour, targetText.val());
+}
+
+// Main
+loadTimeBlocks();
 updateTime();
 setInterval(updateTime, 1000); 
 
+saveBtn.on('click', handleSave);
